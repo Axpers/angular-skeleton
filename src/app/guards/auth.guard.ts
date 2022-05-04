e51194622837b2d81824019a5af1b94541847b7b
GarikD <garikd@outlook.fr>
2022-05-04 17:47:30 +0200

    Add : AuthGuard

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,29 @@
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { Injectable } from '@angular/core';
+import {
+  Router,
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from '@angular/router';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.authService.isLoggedIn()) {
+      // Logged in
+      return true;
+    } else {
+      // Not logged in so redirect to login page with the return url
+      this.router.navigate(['/user/signin'], {
+        queryParams: { returnUrl: state.url },
+      });
+      return false;
+    }
+  }
+}
