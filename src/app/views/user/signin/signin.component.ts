53f426f3497c338fdccad3b0c1346f4bbfa20266
GarikD <garikd@outlook.fr>
2022-05-04 14:35:12 +0200

    Signin form

--- src/app/views/user/signin/signin.component.ts
+++ src/app/views/user/signin/signin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -8,11 +9,20 @@ import { Router } from '@angular/router';
   styleUrls: ['./signin.component.scss'],
 })
 export class SigninComponent implements OnInit {
+  signinForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+
   constructor(private auth: AuthenticationService, private router: Router) {}
 
   ngOnInit() {
     // if (this.auth.isLoggedIn()) {
     //   this.router.navigate(['/']);
     // }
+  }
+
+  login() {
+    console.log(this.signinForm.value);
   }
 }
